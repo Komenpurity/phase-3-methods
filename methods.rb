3f8ead2ded65bf8ef394c1e18455485829e4e778
Komenpurity <jepkorirpurity8@gmail.com>
2023-05-15 12:00:53 +0300

    acompleted assignment

--- methods.rb
+++ methods.rb
@@ -1,1 +1,23 @@
 # Your code here!
+
+def greet_programmer do 
+    puts "Hello, programmer!" 
+end
+
+
+def greet(name) do 
+    puts "Hello, #{name}" 
+end
+
+def greet_with_default(name = "programmer") do 
+    puts "Hello, #{name}" 
+end
+
+
+def add(a,b) do 
+    return a + b 
+end
+
+def halve(a) do 
+    return a / 2
+end
